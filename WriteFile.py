e22f38f7cb5bf8892b83b2296b640631a2f582d9
Kanackle <33528884+Kanackle@users.noreply.github.com>
2022-09-16 10:55:41 -0400

    Delete WriteFile.py

--- WriteFile.py
+++
@@ -1,9 +0,0 @@
-import os
-
-#file_to_save = os.path.join("analysis","election_analysis.txt")
-file_to_save = 'resources/election_analysis.txt'
-outFile = open(file_to_save,'w')
-outFile.write("Counties in the Election\n-----------\n")
-outFile.write("Arapahoe\nDenver\nJefferson")
-
-outFile.close()
